1db282e5a2f33d46cf4662d8adbbbc1c436678d1
Hojin Yoo <hjfirst@gmail.com>
2018-08-27 21:29:43 -0500

    for new env

---
+++ users/yoo@Macbook-Pro/setting-clojure.el
@@ -0,0 +1,69 @@
+;;; setting-clojure.el --- settings for clojure
+;;; Commentary:
+;;; Code:
+
+(require 'setup-clojure-mode)
+; (require 'kubemacider)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; clojure mode hook and helpers
+
+;; (defun clojure-mode-custom-indent ()
+;;   "Custom identation."
+;;   (put-clojure-indent 'fnk 'defun)
+;;   (put-clojure-indent 'defnk 'defun)
+;;   (put-clojure-indent 'for-map 1)
+;;   (put-clojure-indent 'pfor-map 1)
+;;   (put-clojure-indent 'instance 2)
+;;   (put-clojure-indent 'inline 1)
+;;   (put-clojure-indent 'letk 1)
+;;   (put-clojure-indent 'fdef 0))
+
+(defun indent-whole-buffer ()
+  "Indent whole buffer."
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
+
+(defun indent-whole-buffer-c ()
+  "Indent whole buffer."
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
+
+(defun lint-before-save ()
+  "Command for linting."
+  ; (clojure-mode-custom-indent)
+  ; (local-set-key (kbd "C-c C-i") 'indent-whole-buffer)
+  ; (local-set-key (kbd "C-c C-/") 'cider-test-run-ns-tests)
+  ; (setq c-basic-offset 4)
+  ; (setq tab-width 4)
+  (setq indent-tabs-mode nil)
+  (setq cider-auto-select-error-buffer nil)
+
+  (add-hook 'before-save-hook 'indent-whole-buffer nil t)
+  (add-hook 'before-save-hook 'delete-trailing-whitespace))
+(add-hook 'clojure-mode-hook 'lint-before-save)
+
+;; (use-package flycheck-tip
+;;   :ensure t
+;;   (flycheck-tip-use-timer 'verbose))
+
+(flycheck-clojure-setup)
+(use-package flycheck-pos-tip
+  :config (setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
+
+(defun clojure-minor-mode-hook ()
+  "My settings for clojure."
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1)
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
+(add-hook 'clojure-mode-hook 'clojure-minor-mode-hook)
+
+;; from https://github.com/boot-clj/boot/wiki/Cider-REPL
+(setq cider-boot-parameters "cider repl -s -H :: wait")
+
+(provide 'setting-clojure)
+;;; setting-clojure.el ends here
---
+++ users/yoo@Macbook-Pro/setting-misc.el
@@ -0,0 +1,23 @@
+(defun switch-to-previous-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+(global-set-key (kbd "C-;") 'switch-to-prev-buffer)
+
+;; (set-default-font "Source Code Pro" nil t)
+;; (set-face-attribute 'default nil :height 125)
+
+(use-package wgrep)
+(use-package git-link)
+
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config
+  (setq dumb-jump-selector 'ivy)
+  (setq dumb-jump-aggressive nil))
