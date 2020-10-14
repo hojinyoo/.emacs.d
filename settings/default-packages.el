4b474d012b4fcf4ec802f96620da8ae673fd53d2
Hojin Yoo <hjfirst@gmail.com>
2020-10-14 13:15:11 -0400

    remove eshell uninstall pdf-tools add gitignores remove kubemacider

--- settings/default-packages.el
+++ settings/default-packages.el
@@ -163,15 +163,15 @@
   :bind (("C-M-;" . git-link-homepage)
          ("C-M-'" . git-link)))
 
-(use-package pdf-tools
-  :config
-  (pdf-tools-install)
-  ;; open pdfs scaled to fit page
-  (setq-default pdf-view-display-size 'fit-page)
-  ;; automatically annotate highlights
-  (setq pdf-annot-activate-created-annotations t)
-  ;; use normal isearch
-  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
+;; (use-package pdf-tools
+;;   :config
+;;   (pdf-tools-install)
+;;   ;; open pdfs scaled to fit page
+;;   (setq-default pdf-view-display-size 'fit-page)
+;;   ;; automatically annotate highlights
+;;   (setq pdf-annot-activate-created-annotations t)
+;;   ;; use normal isearch
+;;   (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
 
 (require 'setup-org)
 
