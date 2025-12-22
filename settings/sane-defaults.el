47c4ef4bc33718a0430a451c2e16f27bc1b417c7
hojinyoo <hojinyoo@users.noreply.github.com>
2025-12-22 23:05:12 +0900

    misc

--- settings/sane-defaults.el
+++ settings/sane-defaults.el
@@ -1,3 +1,4 @@
+;;; -*- lexical-binding: t -*-
 ;; Allow pasting selection outside of Emacs
 (setq x-select-enable-clipboard t)
 
--- settings/setup-yasnippet.el
+++ settings/setup-yasnippet.el
@@ -1,3 +1,4 @@
+;;; -*- lexical-binding: t -*-
 ;; (add-to-list 'yas-snippet-dirs "~/.emacs.d/yasnippet-snippets")
 
 (yas-global-mode 1)
